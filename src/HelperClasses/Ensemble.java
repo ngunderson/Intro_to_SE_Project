16a4e88ca409b37dc570f879f5df377d5262e459
Nathan P Gunderson <gundersonn@uwplatt.edu>
2015-12-04 13:55:16 -0600

    Implemented initial Ensemble class

--- src/HelperClasses/Ensemble.java
+++ src/HelperClasses/Ensemble.java
@@ -1,10 +1,29 @@
 package HelperClasses;
 
+import java.util.ArrayList;
+
 /**
  
  @author gundersonn
  */
 public class Ensemble 
 {
+   private Director director;
+   private EventList events;
+   private ArrayList<Person> members;
+   private ArrayList<String> messages;
+   private String name;
+   
+   public Ensemble( Director d, EventList e, String n )
+   {
+      director = d;
+      events = e;
+      name = n;
+   }
+   
+   public boolean createEvent( Event e )
+   {  
+      return events.add(e);
+   }
    
 }
